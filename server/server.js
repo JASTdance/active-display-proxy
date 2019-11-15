80455f695d8b441b3299a9e750648f2f17f2199b
Thomas Johnson <thomasij813@gmail.com>
2019-11-15 12:02:18 -0800

    Add remote bundles and styles

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const rp = require('request-promise');
 
 const app = express();
 const port = 3059;
@@ -13,6 +14,13 @@ app.use(express.static(path.resolve(__dirname,'..','public')));
 app.get('/', (_, response) => {
   response.send(express.static(__dirname, '..', 'public', 'index.html'));
 });
+
+app.get('/api/song/:id/comments', (req, res, next) => {
+  const url = `http://localhost:3050/api/song/${req.params.id}/comments`
+  rp(url)
+    .then(data => res.send(data))
+    .catch(err => next(err))
+})
 
 app.listen(port, () => {
   console.log('Proxy server is up and running on port', port);
